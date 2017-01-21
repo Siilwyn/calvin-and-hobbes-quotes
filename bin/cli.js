9466d7018acab2356fd90c3d4b6b0bbcefc82610
Jesse Friedman <jesse@jesse.ws>
2017-01-21 13:02:27 +0100

    Add CLI cowsay option

--- bin/cli.js
+++ bin/cli.js
@@ -6,11 +6,13 @@ const core = require('../src/index.js');
 const fs = require('fs');
 const minimist = require('minimist');
 const path = require('path');
+const cowsay = require('cowsay');
+const wrap = require('wordwrap')(40);
 
 const usage = fs.readFileSync(path.join(__dirname, 'usage.txt'), 'utf8');
 const argv = minimist(process.argv.slice(2), {
-    boolean: [ 'help', 'all' ],
-    alias: { help: 'h', all: 'a' }
+    boolean: [ 'help', 'all', 'say' ],
+    alias: { help: 'h', all: 'a', say: 's' },
 });
 
 if (argv.help) {
@@ -20,6 +22,16 @@ if (argv.help) {
 
 if (argv.all) {
     console.log(core.all);
+    process.exit();
+}
+
+if (argv.say) {
+    console.log(
+        cowsay.say({
+            text: wrap(core.random()),
+            f: path.join(__dirname, 'calvin.cow'),
+        })
+    );
     process.exit();
 }
 
